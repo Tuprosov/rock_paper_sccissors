7c5b50b6d93b713d0b21c048a791805ecf9fd8bd
Shahzod <shahzodwill@gmail.com>
2023-09-06 11:28:18 +0200

    add js logic/ some minor styles

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-function getComptureChoice (arr) {
+function getComputerChoice (arr) {
     return arr[Math.floor(Math.random() * arr.length)] 
 }
 
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = Array.from(document.querySelectorAll('.btn'));
+const result = document.querySelector('.results');
+const roundCounter = document.querySelector('.round-count');
+const player_score = document.querySelector('.player-score');
+const cpu_score = document.querySelector('.cpu-score');
+const resultText = document.querySelector('.results-text')
+let player_scoreNum = 0;
+let cpu_scoreNum = 0;
+let counter = 0;
 
-for (let i = 1; i < 6; i++) {
-    let mychoice = prompt('Your choice: ')
-    let computerChoice = getComptureChoice(choices)
+;
 
-    if (mychoice === computerChoice) {
-        console.log('Draw! Rext round')
-    };
+choices.forEach(player_choice => player_choice.addEventListener('click', function (){
+    let player_str = player_choice.textContent.toLowerCase()
+    let cpu_str = getComputerChoice(choices).textContent.toLowerCase()
 
-    if (
-    (mychoice === 'Rock' && computerChoice === 'Scissors') || 
-    (mychoice === 'Paper' && computerChoice === 'Rock') || 
-    (mychoice === 'Scissors' && computerChoice === 'Paper')
-        ) {
-        console.log(`${mychoice} beats ${computerChoice}, Player won!`)
+
+    counter += 1;
+    roundCounter.textContent = `Round ${counter}`
+
+    if (player_str === cpu_str) {
+        resultText.textContent = 'Tie!'  
     } else {
-        console.log(`${computerChoice} beats ${mychoice}, Computer won!`)
+
+        if (
+            (player_str === 'rock' && cpu_str === 'scissors') ||
+            (player_str === 'paper' && cpu_str === 'rock') ||
+            (player_str === 'scissors' && cpu_str === 'paper')
+        ) {
+            player_scoreNum += 1
+            player_score.textContent = `${player_scoreNum}`
+            resultText.textContent = `${player_str} beats ${cpu_str}. Player wins!`
+
+        } else {
+            cpu_scoreNum += 1
+            cpu_score.textContent = `${cpu_scoreNum}`
+            resultText.textContent = `${cpu_str} beats ${player_str}. Computer wins!`
+        }
     };
-    
-}
+}));
